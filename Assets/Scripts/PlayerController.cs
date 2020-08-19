ace860b06aaa3bc0fdb1c0c08bf376e22fcc0ff6
skite9 <skite@student.unimelb.edu.au>
2020-08-19 22:19:07 +1000

    Player now can't double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpAmount;
+    public GameObject platform;
 
     private Rigidbody rb;
+    private bool onGround = false;
 
     // Start is called before the first frame update
     void Start()
@@ -14,14 +16,36 @@ public class PlayerController : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
+    //private void OnCollisionEnter(Collision collision)
+    //{
+        
+    //}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+
+        //Debug.Log(collision.collider.name == "Platform");
+        //if (Input.GetKeyDown("space") && collision.collider.name == "Platform")
+        //{
+        //    Vector2 jumpPower = new Vector2(0, jumpAmount);
+        //    rb.AddForce(jumpPower);
+        //}
+    }
+
+     void OnCollisionExit(Collision collision)
+    {
+        onGround = false;
+        
+    }
+
     void Update()
     {
-
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && onGround)
         {
             Vector2 jumpPower = new Vector2(0, jumpAmount);
             rb.AddForce(jumpPower);
         }
-
     }
+
 }
