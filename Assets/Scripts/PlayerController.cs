45590288f5f6d3ae6697ab7fcc71b25093ca01ad
skite9 <skite@student.unimelb.edu.au>
2020-08-21 14:00:31 +1000

    Fixed player jumping mechanic using BoxCast.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,43 +6,37 @@ public class PlayerController : MonoBehaviour
 {
     public float jumpAmount;
     public GameObject platform;
+    public LayerMask platformsLayer;
 
     private Rigidbody2D rb;
-    private bool onGround = false;
+    //private bool onGround = false;
+    private BoxCollider2D bcol;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         //Debug.Log(playerGroundHeight);
+        bcol = gameObject.GetComponent<BoxCollider2D>();
     }
 
-    //void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Debug.Log(collision.collider.tag);
+    // https://unitycodemonkey.com/video.php?v=c3iEl5AwUF8
+    private bool groundCheck()
+    {
+        // Create the raycast fromt they player which extends slightly below the player 
+        RaycastHit2D onGround = Physics2D.BoxCast(bcol.bounds.center,
+            bcol.bounds.size, 0f, Vector2.down, bcol.bounds.extents.y + 0.1f, platformsLayer);
 
-    //    if (collision.gameObject.tag == "Platform")
-    //    {
-    //        onGround = true;
-    //    }
-    //    Debug.Log(onGround);
-    //}
+        Debug.Log(onGround.collider);
 
-    //void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    //Debug.Log(collision.collider.name);
-
-    //    if (collision.gameObject.tag == "Platform")
-    //    {
-    //        onGround = false;
-    //    }
-    //    Debug.Log(onGround);
-
-    //}
+        return (onGround.collider != null);
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown("space") && (onGround))
+        if (Input.GetKeyDown("space") && groundCheck())
         {
             Vector2 jumpPower = new Vector2(0, jumpAmount);
             rb.AddForce(jumpPower);
