70482e7e102e068ee4ffa40a188842050241fdca
Waqas Rehmani <dada.scene@hotmail.com>
2020-08-23 16:30:41 +1000

    Changed Color Theme

---
+++ Assets/Scripts/CitySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitySpawner : MonoBehaviour
+{ 
+
+    public GameObject city;
+
+    private float timer = 0;
+    public float spawnTime = 10f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= spawnTime)
+        {
+            
+            Instantiate(city, transform.position, Quaternion.identity);
+            
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
